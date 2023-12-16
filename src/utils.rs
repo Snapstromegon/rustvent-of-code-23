390cdccd84bd65af49ec50e8ca8ff329018e5159
Raphael Höser <raphael@hoeser.info>
2023-12-16 13:48:46 +0100

    Reduce Examples to remove duplicates
    Signed-off-by: Raphael Höser <raphael@hoeser.info>

--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,21 @@
+use std::fs::read_to_string;
+use std::path::Path;
+
 pub fn read_input(day: usize, example: bool, part: u8) -> Option<String> {
-    let filename = format!(
-        "inputs/{:02}{}.txt",
-        day,
-        if example {
-            format!("-example-{part}")
-        } else {
-            "".to_string()
-        }
-    );
-    std::fs::read_to_string(filename).ok()
+    read_to_string(
+        get_possible_paths(day, example, part)
+            .iter()
+            .find(|path| Path::new(path).exists())?,
+    )
+    .ok()
+}
+
+fn get_possible_paths(day: usize, example: bool, part: u8) -> Vec<String> {
+    let mut paths = Vec::new();
+    if example {
+        paths.push(format!("inputs/{:02}-example-{}.txt", day, part));
+        paths.push(format!("inputs/{:02}-example.txt", day));
+    }
+    paths.push(format!("inputs/{:02}.txt", day));
+    paths
 }
